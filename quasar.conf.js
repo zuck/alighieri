a75956dfd35adcd37e78debf5486f6c50a3f8c79
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2021-02-20 14:18:32 +0100

    feat: package electron app with electron-builder

--- quasar.conf.js
+++ quasar.conf.js
@@ -170,7 +170,7 @@ module.exports = function (/* ctx */) {
 
     // Full list of options: https://quasar.dev/quasar-cli/developing-electron-apps/configuring-electron
     electron: {
-      bundler: 'packager', // 'packager' or 'builder'
+      bundler: 'builder', // 'packager' or 'builder'
 
       packager: {
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
@@ -187,7 +187,7 @@ module.exports = function (/* ctx */) {
 
       builder: {
         // https://www.electron.build/configuration/configuration
-        appId: 'alighieri'
+        appId: 'com.alighieri.app'
       },
 
       // More info: https://quasar.dev/quasar-cli/developing-electron-apps/node-integration
