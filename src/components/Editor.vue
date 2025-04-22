4b1e40e65811230d33dc410e71d32b284e3a0440
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2025-04-22 16:11:23 +0200

    feat: add support for links

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -26,7 +26,7 @@
           </q-list>
         </q-menu>
       </q-btn>
-      <q-separator vertical/>
+      <q-separator vertical />
       <q-btn
         dense
         icon="format_align_left"
@@ -45,7 +45,7 @@
         :color="editor.isActive({ textAlign: 'right' }) ? 'accent' : color"
         @click="editor.chain().focus().setTextAlign('right').run()"
       />
-      <q-separator vertical/>
+      <q-separator vertical />
       <q-btn
         dense
         icon="format_bold"
@@ -76,7 +76,13 @@
         :color="editor.isActive('code') ? 'accent' : color"
         @click="editor.chain().focus().toggleCode().run()"
       />
-      <q-separator vertical/>
+      <q-separator vertical />
+      <q-btn
+        dense
+        icon="link"
+        :color="editor.isActive('link') ? 'accent' : color"
+        @click="() => setLink(editor)"
+      />
       <q-btn
         dense
         icon="format_quote"
@@ -91,115 +97,140 @@
     :tippy-options="{ duration: 100 }"
   >
     <q-btn-group flat class="q-ml-lg faded">
-      <q-btn
-        flat
-        round
-        icon="o_add_a_photo"
-        @click="addImage(editor)"
-      />
+      <q-btn flat round icon="o_add_a_photo" @click="addImage(editor)" />
     </q-btn-group>
   </floating-menu>
   <editor-content :class="editorClass" :editor="editor" />
 </template>
 
 <script>
-import CodeBlock from '@tiptap/extension-code-block'
-import Image from '@tiptap/extension-image'
-import TextAlign from '@tiptap/extension-text-align'
-import Typography from '@tiptap/extension-typography'
-import Underline from '@tiptap/extension-underline'
-import StarterKit from '@tiptap/starter-kit'
-import { BubbleMenu, EditorContent, FloatingMenu, useEditor } from '@tiptap/vue-3'
-import { debounce, useQuasar } from 'quasar'
-import { computed } from 'vue'
-import { useI18n } from 'vue-i18n'
-import { useStore } from 'vuex'
-import { UPDATE_STATS_DEBOUNCE_TIME } from '../config'
+import CodeBlock from "@tiptap/extension-code-block";
+import Image from "@tiptap/extension-image";
+import Link from "@tiptap/extension-link";
+import TextAlign from "@tiptap/extension-text-align";
+import Typography from "@tiptap/extension-typography";
+import Underline from "@tiptap/extension-underline";
+import StarterKit from "@tiptap/starter-kit";
+import {
+  BubbleMenu,
+  EditorContent,
+  FloatingMenu,
+  useEditor,
+} from "@tiptap/vue-3";
+import { debounce, useQuasar } from "quasar";
+import { computed } from "vue";
+import { useI18n } from "vue-i18n";
+import { useStore } from "vuex";
+import { UPDATE_STATS_DEBOUNCE_TIME } from "../config";
 
 export default {
   components: {
     EditorContent,
     BubbleMenu,
-    FloatingMenu
+    FloatingMenu,
   },
 
   props: {
     modelValue: {
       type: String,
-      default: ''
-    }
+      default: "",
+    },
   },
 
-  setup (props, { emit }) {
-    const { t } = useI18n()
+  setup(props, { emit }) {
+    const { t } = useI18n();
     const updateContent = debounce((content, text) => {
-      emit('update:modelValue', content)
-      emit('update:text', text)
-    }, UPDATE_STATS_DEBOUNCE_TIME)
+      emit("update:modelValue", content);
+      emit("update:text", text);
+    }, UPDATE_STATS_DEBOUNCE_TIME);
 
-    const quasar = useQuasar()
-    const store = useStore()
-    const color = computed(() => 'black')
+    const quasar = useQuasar();
+    const store = useStore();
+    const color = computed(() => "black");
     const editorClass = computed(() => ({
-      'par-space-between': store.state.base.parSpaceBetween,
-      'par-indent-first-line': store.state.base.parIndentFirstLine
-    }))
+      "par-space-between": store.state.base.parSpaceBetween,
+      "par-indent-first-line": store.state.base.parIndentFirstLine,
+    }));
     const editor = useEditor({
       content: props.modelValue,
       extensions: [
         StarterKit,
+        Link,
         Underline,
         Typography,
         TextAlign.configure({
-          types: ['heading', 'paragraph']
+          types: ["heading", "paragraph"],
         }),
         CodeBlock,
-        Image
+        Image,
       ],
       editorProps: {
         attributes: {
-          class: 'q-py-xl q-px-lg no-outline'
-        }
-      },
-      onUpdate () {
-        updateContent(this.getHTML(), this.getText())
-      }
-    })
-
-    const addImage = (editor) => {
-      quasar.dialog({
-        title: t('Add image'),
-        prompt: {
-          model: '',
-          placeholder: t('Please, enter the image source URL'),
-          type: 'text'
+          class: "q-py-xl q-px-lg no-outline",
         },
-        cancel: true,
-        persistent: true
-      }).onOk(src => {
-        if (src !== null) {
-          editor.chain().focus().setImage({ src }).run()
-        }
-      })
+      },
+      onUpdate() {
+        updateContent(this.getHTML(), this.getText());
+      },
+    });
+
+    function addImage(editor) {
+      quasar
+        .dialog({
+          title: t("Add image"),
+          prompt: {
+            model: "",
+            placeholder: t("Please, enter the image source URL"),
+            type: "text",
+          },
+          cancel: true,
+          persistent: true,
+        })
+        .onOk((src) => {
+          if (src !== null) {
+            editor.chain().focus().setImage({ src }).run();
+          }
+        });
+    }
+
+    function setLink(editor) {
+      const previousUrl = editor.getAttributes("link").href;
+      quasar
+        .dialog({
+          title: t("Set link"),
+          prompt: {
+            model: previousUrl,
+            placeholder: t("Please, enter the link URL"),
+            type: "text",
+          },
+          cancel: true,
+          persistent: true,
+        })
+        .onOk((href) => {
+          href
+            ? editor.chain().focus().setLink({ href }).run()
+            : editor.chain().focus().unsetLink().run();
+        });
     }
 
     return {
       editor,
       editorClass,
       color,
-      addImage
-    }
+      addImage,
+      setLink,
+    };
   },
 
   watch: {
-    modelValue (value) {
-      const isSame = this.editor.getHTML() === value
+    modelValue(value) {
+      const isSame = this.editor.getHTML() === value;
       if (!isSame) {
-        this.editor.commands.setContent(value, true)
+        this.editor.commands.setContent(value, true);
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="sass">
